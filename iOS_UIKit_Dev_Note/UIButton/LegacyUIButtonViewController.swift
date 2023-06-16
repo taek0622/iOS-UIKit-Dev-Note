f0dac14a27d2a8efeedc2b560e9b49e50290c38d
taek0622 <min4644@gmail.com>
2023-06-17 00:19:16 +0900

    Design:iphone:: Add Legacy Button Layout
    - 기존 방식 버튼 레이아웃 추가

--- iOS_UIKit_Dev_Note/UIButton/LegacyUIButtonViewController.swift
+++ iOS_UIKit_Dev_Note/UIButton/LegacyUIButtonViewController.swift
@@ -8,4 +8,32 @@
 import UIKit
 
 class LegacyUIButtonViewController: UIViewController {
+
+    // MARK: - View
+
+    private let legacyButton = UIButton()
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        attribute()
+        layout()
+    }
+
+    // MARK: - Method
+
+    private func attribute() {
+        view.backgroundColor = .white
+    }
+
+    private func layout() {
+        view.addSubview(legacyButton)
+        NSLayoutConstraint.activate([
+            legacyButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            legacyButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            legacyButton.widthAnchor.constraint(equalToConstant: 80),
+            legacyButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
 }
