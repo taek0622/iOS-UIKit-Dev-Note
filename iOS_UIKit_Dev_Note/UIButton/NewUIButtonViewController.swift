e2276733e9d8feae1803ce98f90b067c0086db05
taek0622 <min4644@gmail.com>
2023-06-17 00:22:18 +0900

    Design:iphone:: Add new Button layout
    - 새로운 버튼 방식을 설명하기 위한 뷰 레이아웃 추가

--- iOS_UIKit_Dev_Note/UIButton/NewUIButtonViewController.swift
+++ iOS_UIKit_Dev_Note/UIButton/NewUIButtonViewController.swift
@@ -8,4 +8,30 @@
 import UIKit
 
 class NewUIButtonViewController: UIViewController {
+
+    // MARK: - View
+
+    private lazy var newButton = UIButton()
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        attribute()
+        layout()
+    }
+
+    // MARK: - Method
+
+    private func attribute() {
+        view.backgroundColor = .white
+    }
+
+    private func layout() {
+        view.addSubview(newButton)
+        NSLayoutConstraint.activate([
+            newButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            newButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
 }
